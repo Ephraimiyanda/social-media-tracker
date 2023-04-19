8a637db46097f9d9a7ec758c31b374514aa4a10a
Ephraimiyanda <iyandaephraim@gmail.com>
2023-04-18 16:11:11 -0900

    second commit

--- src/components/props/card.jsx
+++ src/components/props/card.jsx
@@ -4,13 +4,13 @@ function Card(props){
 // const[theme, changeTheme]=useState(false)
 
 return(
-    <div className="card">
+    <div className={props.cardClass}>
         <div className={props.classname}></div>
         <div className="handle">
             <img className="social-media-logo" src={props.src} alt="" />
             <p className="name">@{props.Username}</p>
         </div>
-        <header className="follower-number">{props.followerNumbers}</header>
+        <header className={props.followerAmount}>{props.followerNumbers}</header>
         <p className="followers">{props.followers}</p>
         <div className={props.percentage}><img className="indicator" src={props.upDown} alt="" />{props.percent}  Today</div>
     </div>
--- src/components/props/overview.jsx
+++ src/components/props/overview.jsx
@@ -1,12 +1,12 @@
 function OverView(props){
     return(
-        <div className="overview">
+        <div className={props.overviewClass}>
             <div className="overview-head">
-                <p>{props.views}</p>
+                <p className="">{props.views}</p>
                 <img src={props.src} alt="" />
             </div>
             <div className="overview-body">
-                <p className="views">{props.viewsNo}</p>
+                <p className={props.viewsNoClass}>{props.viewsNo}</p>
                 <div className="viewsPercent"><p className={props.percent}><img className="indicator" src={props.upDown} alt="" />{props.viewsPercent}%</p></div>
             </div>
         </div>
